bc1f88735ffe03ddfdcb21c118ee695a9e09c1a8
Oz <osman.ozdemir@adp.com>
2016-03-12 23:35:03 -0500

    config for load balancer static file sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,12 @@
 var gulp = require('gulp');
 var scp = require('gulp-scp2');
-var ips = [
-  '162.243.99.75',
-  '46.101.185.64',
-  '162.243.205.141',
-  '162.243.205.187',
-  '162.243.205.188',
-  '162.243.205.192'
-];
+var config = require('./config.json');
 
 function onChange(file) {
-  ips.forEach(function(ip){
+  config.destinations.forEach(function(destination){
     gulp
     .src(file.path)
-    .pipe(scp({
-      host: ip,
-      username: 'root',
-      password: 'Kafa1500',
-      dest: '/var/www/foo'
-    }))
+    .pipe(scp(destination))
     .on('error', function(err) {
       console.log(err);
     });
@@ -27,7 +15,7 @@ function onChange(file) {
 
 gulp.task('watch', function() {
   gulp
-  .watch(['foo/**/*.*'])
+  .watch([config.source + '/**/*.*'])
   .on('change', onChange);
 });
 
